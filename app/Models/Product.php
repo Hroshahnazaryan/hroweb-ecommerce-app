242f8ade6a6f3ca2f82afdf9d06c7a0703b0e8c9
Hroshahnazaryan <hrant89@gmail.com>
2021-04-21 23:01:15 +0400

    product model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'sku', 'name', 'slug', 'description', 'quantity',
+        'price', 'discount', 'status', 'featured',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'quantity'  =>  'integer',
+        'status'    =>  'boolean',
+        'featured'  =>  'boolean'
+    ];
+
+    /**
+     * @param $value
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
